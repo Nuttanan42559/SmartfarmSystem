451a70ed720c49c5616cc54d95e001c7dce18c6f
Nuttanan42559 <33050928+Nuttanan42559@users.noreply.github.com>
2019-09-29 23:39:44 +0700

    Add files via upload

---
+++ K2N_Slave_side.ino
@@ -0,0 +1,83 @@
+#include <Wire.h>
+#include <TaskScheduler.h>
+//____PIN_OUT .
+#define H2O 1
+#define Fert1 7
+#define Fert2 8
+#define H2M 6
+#define M2P 5 
+#define LSys 3
+#define FSys 2
+
+//____PIN_IN .
+
+#define wtSeg1 A1
+#define wtSeg2 A2
+
+#define Open LOW
+#define Close HIGH
+
+Scheduler task;
+void task1();
+void task2();
+void task3();
+Task t1(100 * TASK_MILLISECOND, TASK_FOREVER, &task1, &task, true);
+Task t2(100 * TASK_MILLISECOND, TASK_FOREVER, &task2, &task, true);
+Task t3(100 * TASK_MILLISECOND, TASK_FOREVER, &task3, &task, true);
+int dimt = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(5);  
+  Wire.onReceive(receivecall);
+}
+
+void receivecall(int buff){
+  dimt = Wire.read();
+}
+
+void loop() {
+  Serial.println(dimt);
+  
+}
+
+void task1(){
+  switch(dimt){
+    case '101':
+         digitalWrite(LSys, Open);
+         break;
+    case '100':
+         digitalWrite(LSys, Close);
+         break;
+    default:
+         digitalWrite(LSys, Close);
+  }
+}
+
+void task2(){
+  switch(dimt){
+    case '201':
+         digitalWrite(FSys, Open);
+         break;
+    case '200':
+         digitalWrite(FSys, Close);
+         break;
+    default:
+         digitalWrite(FSys, Close);
+  }
+}
+
+void task3(){
+  int wets_1 = analogRead(wtSeg1);
+  int wets_2 = analogRead(wtSeg2);
+  int avg;
+  avg = (wets_1 + wets_2)/2;
+  if(avg >= 840){
+    digitalWrite(H2O, Open);
+    delay(10000);
+  }
+  else if(avg <= 820) {
+    digitalWrite(H2O, Close);
+  }
+}
+
---
+++ K2N_SmartfarmSys_master.ino
@@ -0,0 +1,101 @@
+#include <TaskScheduler.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <BH1750FVI.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#define POT(x) Serial.print(x); 
+#define PUT(x) Serial.println(x); 
+
+#define SlaveNano 5
+
+char ssid_name[] = "SHA256";
+char password[] = "123456789";
+char auth[] = "8e75203d0b2446f09fd580b74f01b392";
+
+Scheduler task;
+void task1();
+void task2();
+Task t1 (100 * TASK_MILLISECOND, TASK_FOREVER, &task1, &task, true);
+Task t2 (100 * TASK_MILLISECOND, TASK_FOREVER, &task2, &task, true);
+BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+DHT dht1(18, DHT22);
+DHT dht2(19, DHT22);
+LiquidCrystal_I2C monitors(0x27, 20, 4);
+
+int state = 0;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid_name, password);
+  Wire.begin();
+  dht1.begin();
+  dht2.begin();
+  LightSensor.begin();
+  monitors.begin();
+  monitors.backlight();
+  //_________________________ .
+  monitors.setCursor(4, 0);
+  monitors.print("K2N. Monitor");
+  monitors.setCursor(0, 1);
+  monitors.print("Temps_in : ");
+  monitors.setCursor(0,2);
+  monitors.print("Temps_out : ");
+  monitors.setCursor(0, 3);
+  monitors.print("Humidity : ");
+  //_________________________ .
+  
+}
+
+void loop() {
+  task.execute();
+}
+
+void task1() {
+  uint16_t lux = LightSensor.GetLightIntensity();
+  if(lux == 20){
+    state = 101;
+    Wire.beginTransmission(SlaveNano);
+    Wire.write(state);
+    Wire.endTransmission();
+  }
+  else { 
+    state = 100;
+    Wire.beginTransmission(SlaveNano);
+    Wire.write(state);
+    Wire.endTransmission();
+  }
+}
+
+void task2() {
+  int temps1 = dht1.readTemperature();
+  int humis1 = dht1.readHumidity();
+  int temps2 = dht2.readTemperature();
+  int humis2 = dht2.readHumidity();
+  monitors.setCursor(11, 1);
+  monitors.print(temps1);
+  monitors.setCursor(12, 2);
+  monitors.print(temps2);
+  monitors.setCursor(11, 3);
+  monitors.print(humis1);
+  if(temps1 > temps2){
+    state = 201;
+    Wire.beginTransmission(9);
+    Wire.write(state);
+    Wire.endTransmission();
+  }
+  else if(temps1 < temps2){
+    state = 200;
+    Wire.beginTransmission(9);
+    Wire.write(state);
+    Wire.endTransmission();
+  }
+}
+
+
+
