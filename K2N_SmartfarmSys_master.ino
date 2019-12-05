edc76ddcbd68fe5b964894c1cd3a22d24c647015
Nuttanan42559 <33050928+Nuttanan42559@users.noreply.github.com>
2019-12-05 10:06:47 +0700

    Add files via upload

--- K2N_SmartfarmSys_master.ino
+++ K2N_SmartfarmSys_master.ino
@@ -1,54 +1,54 @@
 #include <TaskScheduler.h>
 #include <Wire.h>
 #include <DHT.h>
-#include <BH1750FVI.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <WiFi.h>
+//#include <BH1750FVI.h>
+//#include <NTPClient.h>
+//#include <WiFiUdp.h>
+//#include <WiFi.h>
 #include <LiquidCrystal_I2C.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+//#include <WiFiClient.h>
+//#include <BlynkSimpleEsp32.h>
 
 #define POT(x) Serial.print(x); 
 #define PUT(x) Serial.println(x); 
 
 #define SlaveNano 5
 
-char ssid_name[] = "SHA256";
-char password[] = "123456789";
-char auth[] = "8e75203d0b2446f09fd580b74f01b392";
+//char ssid_name[] = "SHA256";
+//char password[] = "123456789";
+//char auth[] = "8e75203d0b2446f09fd580b74f01b392";
 
 Scheduler task;
 void task1();
 void task2();
 Task t1 (100 * TASK_MILLISECOND, TASK_FOREVER, &task1, &task, true);
 Task t2 (100 * TASK_MILLISECOND, TASK_FOREVER, &task2, &task, true);
-BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+//BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
 DHT dht1(18, DHT22);
 DHT dht2(19, DHT22);
-LiquidCrystal_I2C monitors(0x27, 20, 4);
+//LiquidCrystal_I2C monitors(0x27, 20, 4);
 
 int state = 0;
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid_name, password);
+//  WiFi.begin(ssid_name, password);
   Wire.begin();
   dht1.begin();
   dht2.begin();
-  LightSensor.begin();
-  monitors.begin();
-  monitors.backlight();
-  //_________________________ .
-  monitors.setCursor(4, 0);
-  monitors.print("K2N. Monitor");
-  monitors.setCursor(0, 1);
-  monitors.print("Temps_in : ");
-  monitors.setCursor(0,2);
-  monitors.print("Temps_out : ");
-  monitors.setCursor(0, 3);
-  monitors.print("Humidity : ");
-  //_________________________ .
+//  LightSensor.begin();
+//  monitors.begin();
+//  monitors.backlight();
+//  //_________________________ .
+//  monitors.setCursor(4, 0);
+//  monitors.print("K2N. Monitor");
+//  monitors.setCursor(0, 1);
+//  monitors.print("Temps_in : ");
+//  monitors.setCursor(0,2);
+//  monitors.print("Temps_out : ");
+//  monitors.setCursor(0, 3);
+//  monitors.print("Humidity : ");
+//  //_________________________ .
   
 }
 
@@ -57,19 +57,20 @@ void loop() {
 }
 
 void task1() {
-  uint16_t lux = LightSensor.GetLightIntensity();
-  if(lux == 20){
-    state = 101;
-    Wire.beginTransmission(SlaveNano);
-    Wire.write(state);
-    Wire.endTransmission();
-  }
-  else { 
-    state = 100;
-    Wire.beginTransmission(SlaveNano);
-    Wire.write(state);
-    Wire.endTransmission();
-  }
+//  uint16_t lux = LightSensor.GetLightIntensity();
+//  Serial.println(lux);
+//  if(lux < 20){
+//    state = 101;
+//    Wire.beginTransmission(SlaveNano);
+//    Wire.write(state);
+//    Wire.endTransmission();
+//  }
+//  else if(lux > 20){ 
+//    state = 100;
+//    Wire.beginTransmission(SlaveNano);
+//    Wire.write(state);
+//    Wire.endTransmission();
+//  }
 }
 
 void task2() {
@@ -77,25 +78,24 @@ void task2() {
   int humis1 = dht1.readHumidity();
   int temps2 = dht2.readTemperature();
   int humis2 = dht2.readHumidity();
-  monitors.setCursor(11, 1);
-  monitors.print(temps1);
-  monitors.setCursor(12, 2);
-  monitors.print(temps2);
-  monitors.setCursor(11, 3);
-  monitors.print(humis1);
-  if(temps1 > temps2){
+  Serial.println(temps1);
+  Serial.println(temps2);
+//  monitors.setCursor(11, 1);
+//  monitors.print(temps1);
+//  monitors.setCursor(12, 2);
+//  monitors.print(temps2);
+//  monitors.setCursor(11, 3);
+//  monitors.print(humis1);
+  if(temps1 < temps2){
     state = 201;
-    Wire.beginTransmission(9);
+    Wire.beginTransmission(SlaveNano);
     Wire.write(state);
     Wire.endTransmission();
   }
-  else if(temps1 < temps2){
+  else if(temps1 >= temps2){
     state = 200;
-    Wire.beginTransmission(9);
+    Wire.beginTransmission(SlaveNano);
     Wire.write(state);
     Wire.endTransmission();
   }
 }
-
-
-
